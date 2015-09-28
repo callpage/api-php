475ff1ebd4553e2483764b46296166454d885bee
Andriy <andrik.tkachiv@gmail.com>
2015-09-27 23:25:32 -0500

    autoload

---
+++ autoload.php
@@ -0,0 +1,5 @@
+<?php
+//include all
+require dirname(__FILE__) . '/src/exceptions/CallpageRestException.php';
+require dirname(__FILE__) . '/src/Connector.php';
+require dirname(__FILE__) . '/src/Callpage.php';
--- src/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-//include all
-require dirname(__FILE__) . '/exceptions/CallpageRestException.php';
-require dirname(__FILE__) . '/Connector.php';
-require dirname(__FILE__) . '/Callpage.php';
