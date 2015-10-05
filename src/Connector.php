7c737f60003225a0e5eb2612c73658b130c83a14
Andriy <andrik.tkachiv@gmail.com>
2015-10-05 04:59:25 -0500

    ssl support

--- src/Connector.php
+++ src/Connector.php
@@ -12,7 +12,7 @@ class Connector
      * API URL
      * @var string
      */
-    protected $baseUrl = 'http://api.callpage.io/api/v1/external/';
+    protected $baseUrl = 'https://api.callpage.io/api/v1/external/';
     /**
      * API Key for secured connection
      * @var string
