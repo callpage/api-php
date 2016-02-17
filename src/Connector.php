1e30cf8de14ce1aaf539b2b45596de5dbdf2783e
Andrew Tkachiv <andrew.tkachiv@callpage.io>
2016-02-17 17:39:59 +0100

    Fix ssl

--- src/Connector.php
+++ src/Connector.php
@@ -12,7 +12,7 @@ class Connector
      * API URL
      * @var string
      */
-    protected $baseUrl = 'http://api.callpage.io/api/v1/external/';
+    protected $baseUrl = 'api.callpage.io/api/v1/external/';
     /**
      * API Key for secured connection
      * @var string
@@ -24,14 +24,25 @@ class Connector
         $this->apiKey = $apiKey;
     }
 
+    protected function isSSL()
+    {
+        if( !empty( $_SERVER['https'] ) && $_SERVER['https'] !== 'off' )
+            return true;
+
+        if( !empty( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' )
+            return true;
+
+        return false;
+    }
+
     protected function getEndpoint($endpoint, $params = null)
     {
-        return $this->baseUrl . $endpoint . ($params? '?'.http_build_query($params) : '');
+        return ($this->isSSL()? 'https://' : 'http://') . $this->baseUrl . $endpoint . ($params? '?'.http_build_query($params) : '');
     }
 
     protected function getUrl()
     {
-        $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
+        $url  = $this->isSSL() ? 'https://'.$_SERVER["SERVER_NAME"] :  'http://'.$_SERVER["SERVER_NAME"];
         $url .= ( $_SERVER["SERVER_PORT"] != 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
         $url .= $_SERVER["REQUEST_URI"];
         return $url;
